f2dc596b8642c429630255ba0e0bba8b6f74ff20
かとう えい <n17920517@gmail.com>
2018-05-29 22:20:12 +0900

    ファイルがなくても落ちないようにした

--- main.js
+++ main.js
@@ -14,11 +14,19 @@ function addItem() {
   let item = new Item(name, siteAddress);
   let items = readItems();
   items.push(item);
-  fs.writeFileSync("test.txt", JSON.stringify(items));
+  fs.writeFileSync('test.txt', JSON.stringify(items));
 }
 
 function searchItem() {
 
+}
+
+function init() {
+  try {
+    fs.accessSync('test.txt', fs.constants.R_OK | fs.constants.W_OK);
+  } catch (err) {
+    fs.writeFileSync('test.txt', JSON.stringify([]));
+  }
 }
 
 function readItems() {
@@ -29,7 +37,11 @@ function readItems() {
   return JSON.parse(data);
 }
 
+// ------------
+//   main
+// ------------
 const arg1 = process.argv[2];
+init();
 if (arg1 === '-add') {
   addItem();
 } else {
