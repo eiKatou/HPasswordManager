bce27987dd9f562fc410494f7bfd48ede1d5a9ab
かとう えい <n17920517@gmail.com>
2018-06-01 21:33:37 +0900

    リファクタリング

---
+++ src/ItemRepository.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const Item = require('./Item');
+const homeDir = process.env[process.platform == "win32" ? "USERPROFILE" : "HOME"];
+const dataFilePath = homeDir + '/.hpassword_data';
+
+class ItemRepository {
+  static init() {
+    try {
+      fs.accessSync(dataFilePath, fs.constants.R_OK | fs.constants.W_OK);
+    } catch (err) {
+      fs.writeFileSync(dataFilePath, JSON.stringify([]));
+    }
+  }
+
+  static load() {
+    let data = fs.readFileSync(dataFilePath);
+    if (data == '') {
+      return [];
+    }
+    return JSON.parse(data).map(function(element, index, array) {
+      return new Item(element.name, element.site, element.id, element.password);
+    });
+  }
+
+  static write(items) {
+    fs.writeFileSync(dataFilePath, JSON.stringify(items));
+  }
+}
+
+module.exports = ItemRepository;
--- src/main.js
+++ src/main.js
@@ -1,30 +1,29 @@
 const readlineSync = require('readline-sync');
 const fs = require('fs');
 const Item = require('./Item');
+const ItemRepository = require('./ItemRepository');
 
-function addItem() {
-  let name = readlineSync.question('Item name:');
-  let siteAddress = readlineSync.question('Site address:');
-  let id = readlineSync.question('Id:');
-  let password = readlineSync.question('Password:');
+function addItem(items) {
+  let name = readlineSync.question(' Item name:');
+  let siteAddress = readlineSync.question(' Site address:');
+  let id = readlineSync.question(' Id:');
+  let password = readlineSync.question(' Password:');
   let item = new Item(name, siteAddress, id, password);
-  let items = readItems();
   items.push(item);
-  fs.writeFileSync(dataFilePath, JSON.stringify(items));
 }
 
-function searchItem(searchWord) {
-  let items = readItems();
+function searchItem(items, searchWord) {
   let item = findItem(items, searchWord);
   if (item == undefined) {
     console.log('Item not found.');
     return;
   }
+  return item;
+}
 
-  console.log('Item:' + item.name);
-
+function itemCommand(item) {
   for(;;) {
-    let command = readlineSync.question('command: ');
+    let command = readlineSync.question(item.name + ' command: ');
     if (command == 'q' || command == 'quit') {
       break;
     } else if (command == 's' || command == 'show') {
@@ -37,12 +36,12 @@ function searchItem(searchWord) {
   }
 }
 
-function init() {
-  try {
-    fs.accessSync(dataFilePath, fs.constants.R_OK | fs.constants.W_OK);
-  } catch (err) {
-    fs.writeFileSync(dataFilePath, JSON.stringify([]));
+function unlock() {
+  let masterPassword = readlineSync.question('password: ');
+  if (masterPassword == 'ppp') {
+    return true;
   }
+  return false;
 }
 
 function findItem(items, word) {
@@ -51,28 +50,34 @@ function findItem(items, word) {
   });
 }
 
-function readItems() {
-  let data = fs.readFileSync(dataFilePath);
-  if (data == '') {
-    return [];
-  }
-  return JSON.parse(data).map(function(element, index, array) {
-    return new Item(element.name, element.site);
-  });
-}
-
 // ------------
 //   main
 // ------------
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
-const homeDir = process.env[process.platform == "win32" ? "USERPROFILE" : "HOME"];
-const dataFilePath = homeDir + '/.hpassword_data';
-init();
-if (arg1 == '-a' || arg1 == '--add') {
-  addItem();
-} else if (arg1 == '-s' || arg1 == '--search') {
-  searchItem(arg2);
-} else {
-  // TODO:help
+ItemRepository.init();
+let isUnlock = unlock();
+if (!isUnlock) {
+  console.log('Invalid password.');
+  return;
+}
+console.log();
+
+let items = ItemRepository.load();
+for(;;) {
+  let command = readlineSync.question('command: ');
+  if (command == 'q' || command == 'quit') {
+    break;
+  } else if (command == 'a' || command == 'add') {
+    addItem(items); // TODO:副作用ありなので要修正
+    ItemRepository.write(items);
+    console.log();
+  } else if (command == 's' || command == 'search') {
+    let searchWord = readlineSync.question('search: ');
+    let item = searchItem(items, searchWord);
+    if (item == undefined) {
+      break;
+    }
+    console.log();
+    itemCommand(item);
+    console.log();    
+  }
 }
